30472c78f871f03b1f0734feb2738c19c10c4ad4
Hunter Rabun <Hunter.Rabun@gtri.gatech.edu>
2024-10-30 11:12:58 -0400

    Added vscode configs

---
+++ README.md
@@ -0,0 +1,3 @@
+# Configuration Files
+
+This repo contains various configuration files for various softwares
---
+++ vscode/InstallWin.ps1
@@ -0,0 +1,2 @@
+Copy-Item -Path .\keybindings.json -Destination $env:APPDATA\Code\User\
+Copy-Item -Path .\settings.json -Destination $env:APPDATA\Code\User\
